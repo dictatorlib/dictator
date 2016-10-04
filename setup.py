b9851d7800c62e9ca8d4b230c4f04dff2ae599c1
Andrey Maksimov <meamka@ya.ru>
2016-10-04 16:04:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='dictator',
     version='0.1',
-    packages=['dictator'],
+    packages=find_packages(),
     url='https://github.com/amka/dictator',
     license='MIT',
     author='andrey.maksimov',
     author_email='meamka@ya.ru',
-    description='Dictator is a tiny library for Robots to work with Redis as a Dict.'
+    description='Dictator is a tiny library for Robots to work with Redis as a Dict.',
+    install_requires=['redis'],
 )
